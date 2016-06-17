bf20137ab86828c114104851b1ecfc49c1056ace
wencheng <wencheng@ifmuse.com>
2016-06-17 15:16:26 +0800

    工厂报价单入库任务界面

--- module/Quotation/StatusCode.class.php
+++ module/Quotation/StatusCode.class.php
@@ -12,4 +12,19 @@ class Quotation_StatusCode extends SplEnum {
 
     // 生成失败
     const   GENERATEFAILED  = 4;
+
+    /**
+     * 获取所有执行状态代码
+     *
+     * @return array    执行状态
+     */
+    static public function getStatusCode () {
+
+        return  array(
+            self::NOTGERERATE       => '未生成',
+            self::GENERATING        => '生成中',
+            self::GENERATED         => '已生成',
+            self::GENERATEFAILED    => '生成失败',
+        );
+    }
 }
