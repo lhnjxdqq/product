b5d64dddfa08e805d11ea8544b042f12384ab8e9
wencheng <wencheng@ifmuse.com>
2016-06-28 14:27:56 +0800

    解决SPU按规格重量搜索 浮点数精度丢失的问题

--- module/Search/Spu.class.php
+++ module/Search/Spu.class.php
@@ -120,10 +120,17 @@ class Search_Spu {
             ? sprintf('%.2f', $condition['weight_value_end'])
             : 0;
 
-        if ($weightValueEnd && ($weightValueEnd > 0) && ($weightValueEnd >= $weightValueStart)) {
+        if ($weightValueStart == $weightValueEnd && $weightValueStart == 0) {
 
-            $weightRangeList    = range($weightValueStart * 100, $weightValueEnd * 100);
+            $specValueInfo  = Spec_Value_Info::getBySpecValueData('0.00');
+            $specValueId    = $specValueInfo['spec_value_id'];
+            return          '`weight_info`.`spec_value_id` = "' . (int) $specValueId . '"';
+        }
+
+        if ($weightValueEnd && ($weightValueEnd > 0) && ($weightValueEnd >= $weightValueStart)) {
+            $weightRangeList    = range(floor($weightValueStart * 100), ceil($weightValueEnd * 100));
             $weightRangeList    = array_map(create_function('$value', 'return sprintf("%.2f", $value / 100);'), $weightRangeList);
+
             $listSpecValueInfo  = Spec_Value_Info::getByMultiValueData($weightRangeList);
             $listSpecValueId    = array_unique(ArrayUtility::listField($listSpecValueInfo, 'spec_value_id'));
             return              empty($listSpecValueId)
