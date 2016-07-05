54c7a312ac4ad75254b02423375ba7f8005c5879
wencheng <wencheng@ifmuse.com>
2016-07-05 17:47:41 +0800

    生产订单模型 和 生产订单产品关系模型

---
+++ module/Produce/Order/Info.class.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 模型 生产订单
+ */
+class   Produce_Order_Info {
+
+    use Base_MiniModel;
+
+    /**
+     * 数据库配置
+     */
+    const   DATABASE    = 'product';
+
+    /**
+     * 表名
+     */
+    const   TABLE_NAME  = 'produce_order_info';
+
+    /**
+     * 字段
+     */
+    const   FIELDS      = 'produce_order_id,produce_order_sn,produce_order_remark,sales_order_id,delete_status,create_time,update_time';
+    /**
+     * 新增
+     *
+     * @param   array   $data   数据
+     */
+    static  public  function create (array $data) {
+
+        $datetime   = date('Y-m-d H:i:s');
+        $options    = array(
+            'fields'    => self::FIELDS,
+            'filter'    => 'produce_order_id',
+        );
+        $newData    = array_map('addslashes', Model::create($options, $data)->getData());
+        $newData    += array(
+            'create_time'   => $datetime,
+            'update_time'   => $datetime,
+        );
+        self::_getStore()->insert(self::_tableName(), $newData);
+        return self::_getStore()->lastInsertId();
+    }
+
+    /**
+     * 更新
+     *
+     * @param   array   $data   数据
+     */
+    static  public  function update (array $data) {
+
+        $options    = array(
+            'fields'    => self::FIELDS,
+            'filter'    => 'produce_order_id',
+        );
+        $condition  = "`produce_order_id` = '" . addslashes($data['produce_order_id']) . "'";
+        $newData    = array_map('addslashes', Model::create($options, $data)->getData());
+        $newData    += array(
+            'update_time'   => date('Y-m-d H:i:s'),
+        );
+        return  self::_getStore()->update(self::_tableName(), $newData, $condition);
+    }
+}
---
+++ module/Produce/Order/Product/Info.class.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 模型 生产订单产品关系
+ */
+class   Produce_Order_Product_Info {
+
+    use Base_MiniModel;
+
+    /**
+     * 数据库配置
+     */
+    const   DATABASE    = 'product';
+
+    /**
+     * 表名
+     */
+    const   TABLE_NAME  = 'produce_order_product_info';
+
+    /**
+     * 字段
+     */
+    const   FIELDS      = 'produce_order_id,product_id,quantity,remark,delete_status,create_time,update_time';
+    /**
+     * 新增
+     *
+     * @param   array   $data   数据
+     */
+    static  public  function create (array $data) {
+
+        $datetime   = date('Y-m-d H:i:s');
+        $options    = array(
+            'fields'    => self::FIELDS,
+            'filter'    => '',
+        );
+        $newData    = array_map('addslashes', Model::create($options, $data)->getData());
+        $newData    += array(
+            'create_time'   => $datetime,
+            'update_time'   => $datetime,
+        );
+        self::_getStore()->insert(self::_tableName(), $newData);
+        return self::_getStore()->lastInsertId();
+    }
+
+    /**
+     * 更新
+     *
+     * @param   array   $data   数据
+     */
+    static  public  function update (array $data) {
+
+        $options    = array(
+            'fields'    => self::FIELDS,
+            'filter'    => '',
+        );
+        $condition  = "";
+        $newData    = array_map('addslashes', Model::create($options, $data)->getData());
+        $newData    += array(
+            'update_time'   => date('Y-m-d H:i:s'),
+        );
+        return  self::_getStore()->update(self::_tableName(), $newData, $condition);
+    }
+}
