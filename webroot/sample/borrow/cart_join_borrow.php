d5ec73088add826211535fc3113e6be8d20d8532
shuishou <shuishou@ifmuse.com>
2016-08-16 15:00:10 +0800

    合并购物车返回页面修改

--- webroot/sample/borrow/cart_join_borrow.php
+++ webroot/sample/borrow/cart_join_borrow.php
@@ -5,7 +5,7 @@ require dirname(__FILE__).'/../../../init.inc.php';
 $userId         = $_SESSION['user_id'];
 $listGoodsId    = ArrayUtility::listField(Cart_Sample_Info::getByUserId($userId),'goods_id');
 Validate::testNull($_GET['borrow_id'],'借版Id不存在');
-Validate::testNull($listGoodsId,'借版购物车中没有样板,请返回添加','/sample/index.php');
+Validate::testNull($listGoodsId,'借版购物车中没有样板,请返回添加','/sample/borrow/edit.php?borrow_id='.$_GET['borrow_id']);
 
 foreach($listGoodsId as $goodsId){
     
