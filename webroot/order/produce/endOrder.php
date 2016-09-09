0b03031f572cbed5ca1422524969666a4cd9fca1
shuishou <shuishou@ifmuse.com>
2016-09-09 11:14:15 +0800

    结束订单提示

--- webroot/order/produce/endOrder.php
+++ webroot/order/produce/endOrder.php
@@ -6,7 +6,7 @@ Validate::testNull($_GET['produce_order_id'],'订单ID不能为空');
 $produceOrderInfo   = Produce_Order_Arrive_Info::getByProduceOrderId($_GET['produce_order_id']);
 if(!empty(ArrayUtility::searchBy($produceOrderInfo, array('is_storage'=>Produce_Order_Arrive_IsStorage::NO)))){
     
-    Utility::notice('该订单中有未入库的到货单,无法删除');
+    Utility::notice('该订单中有未入库的到货单,无法结束订单');
 }
 Produce_Order_Info::update(array(
     'produce_order_id'  => $_GET['produce_order_id'],
