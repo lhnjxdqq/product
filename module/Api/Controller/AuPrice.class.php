72ea0effc5734fe0f2f1cc82b25ec9f1768012ff
shuishou <shuishou@ifmuse.com>
2016-10-08 18:39:51 +0800

    获取金价借口

--- module/Api/Controller/AuPrice.class.php
+++ module/Api/Controller/AuPrice.class.php
@@ -4,8 +4,9 @@ class Api_Controller_AuPrice {
 
     static public function getNewPrice () {
 
+        $auPrice   = Au_Price_Log::getNewsPrice();
         return array(
-            'auNewPrice' => '205.02'
+            'auNewPrice' => $auPrice,
         );
     }
 
