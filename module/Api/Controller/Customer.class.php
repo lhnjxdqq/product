f891f302baeba737ea3af5df1e2a8da054af0fb8
dalei <dalei@ifmuse.com>
2016-10-08 16:43:57 +0800

    客户列表接口

--- module/Api/Controller/Customer.class.php
+++ module/Api/Controller/Customer.class.php
@@ -4,26 +4,21 @@ class Api_Controller_Customer {
 
     static public function getAll () {
 
-        return array(
-            'CustomerList' => array(
-              array(
-                'customerId'=>1,
-                'customerName'=>'dalei',
-              ),
-              array(
-                'customerId'=>1,
-                'customerName'=>'dalei',
-              ),
-              array(
-                'customerId'=>1,
-                'customerName'=>'dalei',
-              ),
-              array(
-                'customerId'=>1,
-                'customerName'=>'dalei',
-              ),
-            ),
+      $listCustomerInfo = ArrayUtility::searchBy(Customer_Info::listAll(), array('delete_status'=>Customer_DeleteStatus::NORMAL,));
+      $data             = array();
+      $CustomerList     = array();
+      foreach ($listCustomerInfo as $customerInfo) {
+
+        $CustomerList[] = array(
+          'customerId' => $customerInfo['customer_id'],
+          'customerName' => $customerInfo['customer_name'],
         );
+
+      }
+
+      $data['CustomerList'] = $CustomerList;
+      return $data;
+
     }
 
 }
