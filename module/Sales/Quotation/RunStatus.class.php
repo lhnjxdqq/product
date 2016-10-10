5aeaa7193c159efe6fe32219dff2f65b25f6b779
shuishou <shuishou@ifmuse.com>
2016-10-10 15:46:26 +0800

    销售报价单推送

---
+++ module/Sales/Quotation/RunStatus.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ÔËÐÐ×´Ì¬
+ */
+class   Sales_Quotation_RunStatus extends SplEnum {
+
+    const   STANDBY = 1;
+
+    const   RUNNING = 2;
+
+    const   FINISH  = 3;
+
+    const   ERROR   = 4;
+}
