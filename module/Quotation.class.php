f499c16b2601a9f1570e66df12bac25c5320d523
shuishou <shuishou@ifmuse.com>
2016-10-19 21:35:46 +0800

    销售报价单

--- module/Quotation.class.php
+++ module/Quotation.class.php
@@ -1075,10 +1075,6 @@ class   Quotation {
         //获取颜色属性Id列表
         $listSpecValueColotId   = array();
 
-        if(!empty($mapColorInfo)){
-            
-            continue;
-        }
         foreach($mapColorInfo as $spuId=>$colorCost){
             
             foreach($colorCost as $specColorId=>$cost){
