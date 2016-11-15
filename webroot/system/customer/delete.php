abd6eac55e2652afd362e42aae2bab7dc6e50633
shuishou <shuishou@ifmuse.com>
2016-11-15 14:52:46 +0800

    客户管理删除客户

--- webroot/system/customer/delete.php
+++ webroot/system/customer/delete.php
@@ -5,8 +5,22 @@ if (!isset($_GET['customer_id'])) {
 
     Utility::notice('客户ID不能为空');
 }
+$condition  = array(
+    'customer_id'   => $_GET['customer_id'],
+);
+$countSalesOrder        = Sales_Order_Info::countByCondition($condition);
+$countSalesQuotation    = Sales_Quotation_Info::countByCondition($condition);
 
-$data   = array(
+if($countSalesOrder > 0){
+
+    Utility::notice('该客户关联销售订单，请清空相关数据后再操作');
+}
+
+if($countSalesQuotation > 0){
+
+    Utility::notice('该客户关联销售报价单，请清空相关数据后再操作');
+}
+$data       = array(
     'customer_id'           => (int) $_GET['customer_id'],
     'delete_status'         => Customer_DeleteStatus::DELETED,
 );
