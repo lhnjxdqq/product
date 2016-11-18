f8d99f36445aa1ac63e9b806e40906550c499177
shuishou <shuishou@ifmuse.com>
2016-11-18 18:09:38 +0800

    客户二维码图片

--- module/Api/Controller/Customer.class.php
+++ module/Api/Controller/Customer.class.php
@@ -7,8 +7,13 @@ class Api_Controller_Customer {
       $listCustomerInfo = ArrayUtility::searchBy(Customer_Info::listAll(), array('delete_status'=>Customer_DeleteStatus::NORMAL,));
       $data             = array();
       $CustomerList     = array();
+       
       foreach ($listCustomerInfo as $customerInfo) {
-
+      
+        $imageUrl  = !empty($customerInfo['qr_code_image_key'])
+                ? AliyunOSS::getInstance('images-spu')->url($customerInfo['qr_code_image_key'])
+                : '';
+                
         $CustomerList[] = array(
           'customerId'          => $customerInfo['customer_id'],
           'customerName'        => $customerInfo['customer_name'],
