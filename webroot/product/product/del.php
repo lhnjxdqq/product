892b933974a8a09e3374849db969ac9b2fbb725c
shuishou <shuishou@ifmuse.com>
2016-12-23 16:11:12 +0800

    批量删除产品

--- webroot/product/product/del.php
+++ webroot/product/product/del.php
@@ -21,8 +21,9 @@ if (Product_Info::setDeleteStatusByMultiProductId($productIdList, Product_Delete
         // 执行SKU下架操作
         Goods_Info::setOnlineStatusByMultiGoodsId($listOffLineGoodsId, Goods_OnlineStatus::OFFLINE);
         foreach ($listOffLineGoodsId as $goodsId) {
-
-            Goods_Push::changePushGoodsDataStatus($goodsId, 'offline');
+            
+            Sync::queueSkuData($goodsId);
+            Goods_Push::changePushGoodsDataStatus($goodsId, 'offline');    
         }
 
         // 查这些下架的SKU关联了哪些SPU
@@ -35,6 +36,9 @@ if (Product_Info::setDeleteStatusByMultiProductId($productIdList, Product_Delete
             Spu_Info::setOnlineStatusByMultiSpuId($listOffLineSpuId, Spu_OnlineStatus::OFFLINE);
             foreach ($listOffLineSpuId as $spuId) {
 
+                Sync::queueSpuData($spuId);
+                $spuInfo = Spu_Info::getById($spuId);
+                Spu_Push::pushTagsListSpuSn(array($spuInfo['spu_sn']), array('onlineStatus'=>Spu_OnlineStatus::OFFLINE));
                 Spu_Push::changePushSpuDataStatus($spuId, 'offline');
             }
         }
