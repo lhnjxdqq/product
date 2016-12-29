0777cc3b3c3b97934900c510aaf242eb14984ddf
shuishou <shuishou@ifmuse.com>
2016-12-29 18:37:48 +0800

    添加商品类型

--- webroot/manage/goods_type/add.php
+++ webroot/manage/goods_type/add.php
@@ -8,15 +8,24 @@ if(empty($_POST['goods_type_name'])){
     exit;
 }
 
-$listGoodsType      = ArrayUtility::searchBy(Goods_Type_Info::listAll(),array('delete_status'=>0));
-
-$goodsTypeInfo      = ArrayUtility::SearchBy($listGoodsType, array('goods_type_name'=>$_POST['goods_type_name']));
+$goodsTypeInfo      = ArrayUtility::searchBy(Goods_Type_Info::listAll(), array('goods_type_name'=>$_POST['goods_type_name']));
 
 if(!empty($goodsTypeInfo)){
 
-    Utility::notice("商品类型名称重复");
-    exit;
+    $info  = current($goodsTypeInfo);
+    if($info['delete_status'] == 0){
+        
+        Utility::notice("商品类型名称重复");
+        exit;   
+    }else {
+        Goods_Type_Info::update(array(
+            'goods_type_id' => $info['goods_type_id'],
+            'delete_status' => 0,
+        ));
+        Utility::notice("添加完成");
+        exit;
+    }
 }
 
-Goods_Type_Info::create(array('goods_type_name'    => $_POST['goods_type_name']));
+Goods_Type_Info::create(array('goods_type_name'=> $_POST['goods_type_name']));
 Utility::notice("添加完成");
