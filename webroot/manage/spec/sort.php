cd097613ccedb02e96dc4353bd65b661a969d095
shuishou <shuishou@ifmuse.com>
2016-12-30 16:54:20 +0800

    排序失效

--- webroot/manage/spec/sort.php
+++ webroot/manage/spec/sort.php
@@ -3,9 +3,10 @@
 require_once dirname(__FILE__).'/../../../init.inc.php';
 
 Validate::testNull($_POST['spec_value_id'],'数据不能为空');
+
 foreach($_POST['spec_value_id'] as $key => $specValueId){
     
-    Spec_Value_Info::create(array(
+    Spec_Value_Info::update(array(
         'spec_value_id' => $specValueId,
         'serial_number' => $key,
     ));
